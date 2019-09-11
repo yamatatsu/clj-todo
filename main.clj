6c61e782755c03adb7b7b62b3e78605e6ff5c6d6
yamatatsu <the.kinnikuman@gmail.com>
2019-09-11 21:09:24 +0900

    refactor

--- main.clj
+++ main.clj
@@ -1,26 +1,26 @@
-(defn render [type tasks]
-  (case type
-    "List" tasks
-    "Add" ["Input your new task:"]
-    "Done" (conj tasks "Select done task:")
-    "Exit" ["Bye"]))
-
-(defn choice-next []
-  (println "What? (list:'l', add:'a', done:'d', exie: 'e')")
-  (case (read-line)
+(defn get-type [abbreviation]
+  (case abbreviation
     "l" "List"
     "a" "Add"
     "d" "Done"
     "e" "Exit"
     ))
 
+(defn format-for-render [arr]
+  (clojure.string/join "\n" (concat [""] arr)))
+
 (defn recurse [tasks]
-  (let [type (choice-next)]
-    (doseq [word (render type tasks)] (println word))
-    (case type
-      "List" (recurse tasks)
-      "Add" (recurse (conj tasks (read-line)))
-      "Done" (recurse (disj tasks (read-line)))
-      "Exit")))
+  (println "\nWhat? (list:'l', add:'a', done:'d', exie: 'e')")
+  (case (get-type (read-line))
+    "List" (do
+      (println (format-for-render tasks))
+      (recurse tasks))
+    "Add" (do
+      (println "\nInput your new task:")
+      (recurse (conj tasks (read-line))))
+    "Done" (do
+      (println (str (format-for-render tasks) "\nSelect done task:"))
+      (recurse (disj tasks (read-line))))
+    "Exit" (println "\nBye!")))
 
 (recurse #{})

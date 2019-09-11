711b3fd4adf54ac2da22aeb5e7e8fbfec416a6d5
yamatatsu <the.kinnikuman@gmail.com>
2019-09-11 21:31:00 +0900

    refactor

--- main.clj
+++ main.clj
@@ -6,20 +6,25 @@
     "e" "Exit"
     ))
 
-(defn format-for-render [arr]
-  (clojure.string/join "\n" (concat [""] arr)))
+(defn format-tasks [tasks & more]
+  (clojure.string/join
+    "\n"
+    (flatten [
+      ""
+      (vec tasks)
+      more])))
 
 (defn recurse [tasks]
   (println "\nWhat? (list:'l', add:'a', done:'d', exie: 'e')")
   (case (get-type (read-line))
     "List" (do
-      (println (format-for-render tasks))
+      (println (format-tasks tasks))
       (recurse tasks))
     "Add" (do
       (println "\nInput your new task:")
       (recurse (conj tasks (read-line))))
     "Done" (do
-      (println (str (format-for-render tasks) "\nSelect done task:"))
+      (println (format-tasks tasks "Select done task:"))
       (recurse (disj tasks (read-line))))
     "Exit" (println "\nBye!")))
 
